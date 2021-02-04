6b96f7ace2ea8a9329e5a3f09475de1c313e2a4b
Aditya Chandeliya <adityakumar@giddh.com>
2021-02-05 03:37:45 +0530

    chandeliya | moving SpringbootstarterApplication.java into a package & defined the component scan for the project package

--- src/main/java/com/example/springbootstarter/springboot/SpringbootstarterApplication.java
+++ src/main/java/com/example/springbootstarter/springboot/SpringbootstarterApplication.java
@@ -1,11 +1,13 @@
-package com.example.springbootstarter;
+package com.example.springbootstarter.springboot;
 
 import com.example.springbootstarter.controller.GreetController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.springbootstarter")
 public class SpringbootstarterApplication {
 
 	public static void main(String[] args) {
